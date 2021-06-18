5b61c37083c304254d9e361018116cf2a3569ee9
Prakhar Singh <singhprakhar973@gmail.com>
2021-06-18 18:54:09 +0530

    Initials were created

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Weather from './app_component/weather.component'
+
+function App() {
+  return (
+    <div className="App">
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
